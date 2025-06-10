64626bdff23d6f6f67bc57c8e8ffd16b38e68a60
Engineer Abdullah Iqbal <hassangb544@gmail.com>
2025-06-10 08:02:59 +0500

    Updated output_guardrails.py

--- 00_Quiz_preparation/04_guardrails/output_guardrails.py
+++ 00_Quiz_preparation/04_guardrails/output_guardrails.py
@@ -1,57 +1,58 @@
-from pydantic import BaseModel
+import json
+from pydantic import BaseModel, Field
 from config import config
 import asyncio
-from agents import Agent, GuardrailFunctionOutput , OutputGuardrailTripwireTriggered, RunContextWrapper , Runner, TResponseInputItem , output_guardrail
+from agents import Agent, GuardrailFunctionOutput, OutputGuardrailTripwireTriggered, RunContextWrapper , Runner, TResponseInputItem , output_guardrail
 
 
-class OutputResponse(BaseModel):
-    response : str
+class MessageOutput(BaseModel):
+    reasoning: str = Field(description="Thoughts on how to respond to the user's message")
+    response: str = Field(description="The response to the user's message")
+    user_name: str | None = Field(description="The name of the user who sent the message, if known")
 
-class ContentCheckerOutput(BaseModel):
-    is_fake_content : bool
-    reason : str
-
-
-
-
-output_guardrail_agent = Agent(
-    name="output_checker",
-    instructions="You are an output guardrail agent. Your job is to check if the output is appropriate. don't allow any output that is inappropriate, harmful, or offensive. If the output is appropriate, return it as is. If not, return a message saying 'Output not allowed'.",
-    output_type=ContentCheckerOutput,
-)
-
-## For Output Guardrail
 
 @output_guardrail
-async def fake_content_output_guardrail(ctx : RunContextWrapper[None] , agent: Agent , output : OutputResponse
-                                   ) -> GuardrailFunctionOutput:
-    result = await Runner.run(output_guardrail_agent, output.response, run_config=config)
+async def sensetive_data_checker(ctx: RunContextWrapper, agent: Agent, output: MessageOutput
+                                )-> GuardrailFunctionOutput:
+    
+    
+    phone_number_in_response = "650" in output.response
+    phone_number_in_reasoning = "650" in output.reasoning
+
 
     return GuardrailFunctionOutput(
-        tripwire_triggered=result.final_output.is_fake_content,
-        output_info=result.final_output
+        output_info={
+            "phone_number_in_response": phone_number_in_response,
+            "phone_number_in_resoning": phone_number_in_reasoning,
+        },
+        tripwire_triggered= phone_number_in_response or phone_number_in_reasoning
     )
 
 
 
-
-video_maker_agent = Agent(
-    name="video_maker",
-    instructions="you are a video creation assistant help users create engaging videos.",
-    output_guardrails=[fake_content_output_guardrail],
-    output_type=OutputResponse
+agent=Agent(
+    name="assistant",
+    instructions="You are a helpful assistant",
+    output_type=MessageOutput,
+    output_guardrails=[sensetive_data_checker],
+  
 )
 
 
 async def main():
+
+    await Runner.run(agent, "waht is the capital of Pakistan " , run_config=config)
+    print("First Run Passed") 
+
     # This should trip the guardrail
     try:
-        result = await Runner.run(video_maker_agent, "can u make fake video content?" , run_config=config)
-        print("Guardrail didn't trip - this is unexpected")
-        print(result.final_output)
+        result = await Runner.run(agent, "My phone number is 650-123-4567. Where do you think I live?" , run_config=config)
+        print(
+            f"Guardrail didn't trip - this is unexpected : {json.dumps(result.final_output.model_dump(), indent=4)}"
+            )
 
-    except OutputGuardrailTripwireTriggered:
-        print("Fake content output guardrail tripped")
+    except OutputGuardrailTripwireTriggered as e:
+        print(f"Guardrail tripped: {e.guardrail_result.output.output_info}")
 
     
 if __name__ == "__main__":
